491c76c27b6d3859d55e1d2bba9322481b013279
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2023-02-15 03:43:35 +0200

    Added more useful functions/types

--- API Reference file/scriptable_computer.lua
+++ API Reference file/scriptable_computer.lua
@@ -1,13 +1,20 @@
+--- @copyright FlooferLand, TheFattestCat, Donyti_Qramixy
+-- This API reference file is under the MIT license, FlooferLand@2023 - Credit if redistributing!
+-- ScriptableComputer's API does not fall under my copyright.
+-- You don't need to credit me in your mods/creations if you use my tools! Though it would be appreciated! c:
+
+----------------------------------------------------------------
 -- ScriptableComputer & API by TheFattestCat & Donyti_Qramixy
 -- API reference file written by FlooferLand
+----------------------------------------------------------------
 
 -- This API reference file is still being updated from time to time.
 -- Some things might not have documentation or might be included in
 -- Feel free to open up an `Issue` on my project's Github!
 
--- /!\ **DO NOT** bother the developers of the ScriptableComputer mod
--- --- They're not the ones maintaining this API reference file.
--- --- Open up an issue on this project's GitHub instead!
+-- **DO NOT** bother the developers of the ScriptableComputer mod
+--   - They're not the ones maintaining this API reference file.
+--   - Open up an issue on this project's GitHub instead!
 -- https://github.com/FlooferLand/ScriptableComputerUtils/issues
 
 -- /!\ Some functions might return something /!\
@@ -21,68 +28,68 @@
 -- https://github.com/FlooferLand/ScriptableComputerUtils
 ----------------------------------------------
 
---- The Display data structure
+--- The Display data structure.
 Display = {
 
 	--- Gets the width of the display
-	--- @returns number
-	getWidth = function() end,
+	--- @return integer
+	getWidth = function() return(0) end,
 
 	--- Gets the height of the display
-	--- @returns number
-	getHeight = function() end,
+	--- @return integer
+	getHeight = function() return(0) end,
 
 	--- Clears the display's screen using a colour
 	--- @param color string
 	clear = function(color) end,
 
 	--- Draws a pixel of color `color` at a location (`x`, `y`)
-	--- @param x number
-	--- @param y number
+	--- @param x integer
+	--- @param y integer
 	--- @param color string
 	drawPixel = function(x, y, color) end,
 	
 	--- Draws a line-based rectangle of colour `color` at the location of `x` and `y`, with a width/height of `w`/`h`
-	--- @param x number
-	--- @param y number
-	--- @param w number
-	--- @param h number
+	--- @param x integer
+	--- @param y integer
+	--- @param w integer
+	--- @param h integer
 	--- @param color string
 	drawRect = function(x, y, w, h, color) end,
 
 	--- Draws a filled-in rectangle of colour `color` at the location of `x` and `y`, with a width/height of `w`/`h`
-	--- @param x number
-	--- @param y number
-	--- @param w number
-	--- @param h number
+	--- @param x integer
+	--- @param y integer
+	--- @param w integer
+	--- @param h integer
 	--- @param color string
 	fillRect = function(x, y, w, h, color) end,
 
 	--- Draws a line-based circle of colour `color` at the location of `x` and `y`, with a radius of `r`
-	--- @param x number
-	--- @param y number
+	--- @param x integer
+	--- @param y integer
 	--- @param r number
 	--- @param color string
 	drawCircle = function(x, y, r, color) end,
 
 	--- Draws a filled-in circle of colour `color` at the location of `x` and `y`, with a radius of `r`
-	--- @param x number
-	--- @param y number
+	--- @param x integer
+	--- @param y integer
 	--- @param r number
 	--- @param color string
 	fillCircle = function(x, y, r, color) end,
 	
 	--- Draws a line between two locations (`x` `y`, and `x2` `y2`) with a colour of `color`
-	--- @param x number
-	--- @param y number
-	--- @param x2 number
-	--- @param y2 number
+	--- @param x  integer
+	--- @param y  integer
+	--- @param x2 integer
+	--- @param y2 integer
 	--- @param color string
 	drawLine = function(x, y, x2, y2, color) end,
 
 	--- Draws text at the location of `x` `y`, with a colour of `color`, and the text of `text`
-	--- @param x number
-	--- @param y number
+	--- @param x integer
+	--- @param y integer
 	--- @param text string
 	--- @param color string
 	drawText = function(x, y, text, color) end,
@@ -100,7 +107,7 @@ Display = {
 	setClicksAllowed = function(bool) end,
 	
 	--- `TODO:` Documentation missing for this function!
-	--- @return number
+	--- @return integer
 	getClicksAllowed = function() return(0) end,
 	
 	--- `TODO:` Documentation missing for this function! <br/>
@@ -109,13 +116,16 @@ Display = {
 	--- ```Lua
 	--- { x, y, "pressed"|"drag"|"released", 1|2 }
 	--- ```
-	--- @return table[number, number, string, number]
-	getClick = function() return({0,0, "pressed", 1}) end,
+	--- @return table[integer, integer, string, integer]
+	getClick = function() return({0,0,"pressed",1}) end,
 	
-	-- setMaxClicks(n: int [1; 16]), -- sets the buffer size
+	--- Sets the buffer size <br/>
+	--- `n` - [1; 16]
+	--- @param n integer
+	setMaxClicks = function(n) end,
 	
-	--- `TODO:` Documentation missing for this function! <br/>
-	--- @return number
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	--- @return integer
 	getMaxClicks = function() return(0) end,
 	
 	--- Clears the click buffer
@@ -127,15 +137,202 @@ Display = {
 	setRenderAtDistance = function(bool) end,
 
 	--- "Not sure what this means, but I'm guessing it has to do with how far away the screens render" - FlooferLand
-	--- @param bool boolean
+	--- @return boolean
 	getRenderAtDistance = function() return(false) end
+}
+
+--- The StepperMotor data structure
+StepperMotor = {
+	--- Gets the velocity of the stepper motor
+	--- @return number
+	getVelocity = function() return(0) end,
+	
+	--- Sets the velocity of the stepper motor
+	--- @param velocity number
+	setVelocity = function(velocity) end,
+	
+	--- Get the strength of the stepper motor
+	--- @return number
+	getStrength = function() return(0) end,
+
+	--- Sets the strength of the stepper motor
+	--- @param strength number
+	setStrength = function(strength) end,
+	
+	--- Gets the angle the stepper motor is at
+	--- @return number
+	getAngle = function() return(0) end,
+	
+	--- Sets the angle the stepper motor is at
+	--- @param angle number|nil
+	setAngle = function(angle) end,
+	
+	--- Returns true if the stepper motor is active, false if not
+	isActive = function() end,
+	
+	--- Activates/deactivates the stepper motor
+	--- @param active number|boolean
+	setActive = function(active) end
+}
+
+--- The StepperMotor data structure
+Radar = {
+	-- TODO: Verify the types to make sure numbers (floats) aren't integers, and visa versa.
+	--- Returns an array of { body id, horizontal angle, vertical angle, distance, force }
+	--- @return table[body_id, number, number, number, number]
+	getTargets = function() return({0, 0}) end,
+
+	--- Set the angle
+	--- @param angle number
+	setAngle = function(angle) end,
+
+	--- Get the angle
+	--- @return number
+	getAngle = function() return(0) end,
+
+	--- Set the horizontal FoV (from 0 to Pi)
+	--- @param fov number
+	setHFov = function(fov) end,
+
+	--- Get the horizontal FoV (from 0 to Pi)
+	getHFov = function() end,
+
+	--- Set the vertical FoV (from 0 to Pi)
+	--- @param fov number
+	setVFov = function(fov) end,
+
+	--- Get the vertical FoV (from 0 to Pi)
+	getVFov = function() end
+}
+
+--- The NetworkPort data structure
+NetworkPort = {
+	--- Gets how many packets the port can contain
+	--- @return integer
+	getMaxPacketsCount = function() return(0) end,
+	
+	--- Gets how many packets the port currently contains
+	--- @return integer
+	getPacketsCount = function() return(0) end,
+	
+	--- Gives the next packet to the process
+	nextPacket = function() end,
+	
+	--- Sends some string data to the port
+	--- @param data string
+	send = function(data) end,
+	
+	--- Clears the buffered packets
+	clear = function() end
+}
+
+--- The Disk data structure <br/>
+--- `TODO`: Currently not finished!!!
+Disk = {
+	--- Creates a file at a path
+	--- @param path string
+	createFile = function(path) end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	--- @param path string
+	readFile = function(path) end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	--- @param path string
+	writeFile = function(path) end,
+	
+	--- Deletes the file at a path
+	--- @param path string
+	deleteFile = function(path) end,
+	
+	--- Returns true if there's a file at the path, else returns false
+	--- @param path string
+	--- @return boolean
+	hasFile = function(path) return(false) end,
+	
+	--- Returns the size of the file at a path
+	--- @param path string
+	--- @return number|integer
+	getFileSize = function(path) return(0) end,
+	
+	--- Creates a folder at a path
+	--- @param path string
+	createFolder = function(path) end,
+	
+	--- Deletes the folder at a path
+	--- @param path string
+	deleteFolder = function(path) end,
+	
+	--- Returns true if there's a folder at the path, else returns false
+	--- @param path string
+	--- @return boolean
+	hasFolder = function(path) return(false) end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	getUsedSize = function() end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	--- @param path string
+	getFileList = function(path) end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	--- @param path string
+	getFolderList = function(path) end,
+	
+	--- Changes the default tracing path _(e.g. the Windows/Linux `cd` command)_
+	--- @param path string
+	openFolder = function(path) end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	--- @return string
+	getCurrentPath = function() return("") end
+}
+
+--- The Raycasting Camera data structre <br/>
+RaycastingCamera = {
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	drawColor = function(display) end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	drawDepth = function(display, baseColor) end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	drawColorWithDepth = function(display) end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute! <br/>
+	-- `Original description:`  sets the rendering step (how much pixel will be rendered at draw call)
+	setStep = function(int) end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	getStep = function() end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	setDistance = function(num) end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	getDistance = function(num) end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute! <br/>
+	-- `Original description:`  when low it has more details, when high it has more coverange
+	setFov = function(num) end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute!
+	getFov = function() end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute! <br/>
+	--- `Original description:`  get pixel camera starts render from
+	getNextPixel = function() end,
+	
+	--- `TODO:` This function hasn't been documented yet! <br/> Feel free to contribute! <br/>
+	--- `Original description:`  resets that pixel to 0
+	resetCounter = function() end
 }
 
 
 --- Get a value from a register
 --- @param name string
---- @returns number|boolean
-function getreg(name) end
+--- @return number|boolean
+function getreg(name) return(0) end
 
 --- Set a value to a register
 --- @param name string
@@ -146,32 +343,32 @@ function setreg(name, value) end
 function clearregs() end
 
 --- Returns a table of parent computers
---- @returns table
-function getParentComputers() end
+--- @return table
+function getParentComputers() return({}) end
 
 --- Returns a table of child computers
---- @returns table
-function getChildComputers() end
+--- @return table
+function getChildComputers() return({}) end
 
 --- Returns a table of child displays
---- @returns table[Display]
-function getDisplays() end
+--- @return table[Display]
+function getDisplays() return({}) end
 
 --- Returns a table of child motors
---- @returns table
-function getMotors() end
+--- @return table[StepperMotor]
+function getMotors() return({}) end
 
 --- Returns a table of child radars
---- @returns table
-function getRadars() end
+--- @return table[StepperMotor]
+function getRadars() return({}) end
 
 --- Returns a table of child network ports
---- @returns table
-function getPorts() end
+--- @return table
+function getPorts() return({}) end
 
---- Returns a table of child discks
---- @returns table
-function getDisks() end
+--- Returns a table of child disks
+--- @return table
+function getDisks() return({}) end
 
 --- Invokes some code on all the clients
 --- @param code string
@@ -180,15 +377,16 @@ function clientInvoke(code) end
 --- Input selector (if a color is specified);
 --- Returns true if one of the selected inputs is true, else returns false
 --- @param color string|nil
---- @returns bool
-function input(color) end
+--- @return boolean
+function input(color) return(false) end
 
 --- If a colour is specified it returns a table of parent interactable powers;
 --- If there is no color it returns a full table of powers
 --- @param color string|nil
---- @returns table[number]
-function ninput(color) end
+--- @return table[number]
+function ninput(color) return({0}) end
 
+-- TODO: Evaluate if this is a number, or an integer
 --- Set the power and active state of `self.interactable`
 --- @param value number|boolean
 function out(value) end
@@ -219,4 +417,3 @@ function execute(code, env) end
 
 -- --- deprecated, use getRadars
 -- function getConnectedRadarsData() end
-
